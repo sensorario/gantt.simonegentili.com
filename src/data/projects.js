55184e3c719cdc4b40fbf774ff460440040b3cfe
Simone Gentili <sensorario@gmail.com>
2026-07-03 15:39:53 +0200

    chore(config): update data

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,4 +1,4 @@
-import { Temporal } from '@js-temporal/polyfill'
+import { Temporal } from "@js-temporal/polyfill";
 
 /**
  * Dati dei progetti e dei task.
@@ -11,37 +11,103 @@ import { Temporal } from '@js-temporal/polyfill'
  */
 
 export const initialProjects = [
-    {
-        id: 3,
-        name: 'Scrivere',
-        tasks: [
-            {
-                id: 234,
-                name: 'JavaScript // II edition',
-                start: Temporal.PlainDate.from('2026-01-01'),
-                end: Temporal.PlainDate.from('2026-06-01'),
-            },
-            {
-                id: 987,
-                name: 'TypeScript // II edition',
-                start: Temporal.PlainDate.from('2026-06-01'),
-                end: Temporal.PlainDate.from('2026-09-01'),
-                dependsOn: 234,
-            },
-            {
-                id: 456,
-                name: 'JavaScript // III edition',
-                start: Temporal.PlainDate.from('2026-09-01'),
-                end: Temporal.PlainDate.from('2027-08-31'),
-                dependsOn: 987,
-            },
-            {
-                id: 988,
-                name: 'React // II edition',
-                start: Temporal.PlainDate.from('2026-09-01'),
-                end: Temporal.PlainDate.from('2027-09-01'),
-                dependsOn: 987,
-            },
-        ],
-    },
-]
+  {
+    id: 1,
+    name: "JavaScript",
+    tasks: [
+      {
+        id: 101,
+        name: "Edit del master",
+        start: Temporal.PlainDate.from("2026-01-01"),
+        end: Temporal.PlainDate.from("2026-05-01"),
+      },
+      {
+        id: 102,
+        name: "Pubblicazione paperback",
+        start: Temporal.PlainDate.from("2026-05-01"),
+        end: Temporal.PlainDate.from("2026-06-01"),
+        dependsOn: 101,
+      },
+      {
+        id: 103,
+        name: "Conversione per ebook",
+        start: Temporal.PlainDate.from("2026-05-01"),
+        end: Temporal.PlainDate.from("2026-06-15"),
+        dependsOn: 101,
+      },
+      {
+        id: 104,
+        name: "Pubblicazione ebook",
+        start: Temporal.PlainDate.from("2026-06-15"),
+        end: Temporal.PlainDate.from("2026-07-01"),
+        dependsOn: 103,
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "TypeScript",
+    tasks: [
+      {
+        id: 201,
+        name: "Edit del master",
+        start: Temporal.PlainDate.from("2026-06-01"),
+        end: Temporal.PlainDate.from("2026-09-01"),
+      },
+      {
+        id: 202,
+        name: "Pubblicazione paperback",
+        start: Temporal.PlainDate.from("2026-09-01"),
+        end: Temporal.PlainDate.from("2026-10-01"),
+        dependsOn: 201,
+      },
+      {
+        id: 203,
+        name: "Conversione per ebook",
+        start: Temporal.PlainDate.from("2026-09-01"),
+        end: Temporal.PlainDate.from("2026-10-15"),
+        dependsOn: 201,
+      },
+      {
+        id: 204,
+        name: "Pubblicazione ebook",
+        start: Temporal.PlainDate.from("2026-10-15"),
+        end: Temporal.PlainDate.from("2026-11-01"),
+        dependsOn: 203,
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "React",
+    tasks: [
+      {
+        id: 301,
+        name: "Edit del master",
+        start: Temporal.PlainDate.from("2026-09-01"),
+        end: Temporal.PlainDate.from("2027-06-01"),
+      },
+      {
+        id: 302,
+        name: "Pubblicazione paperback",
+        start: Temporal.PlainDate.from("2027-06-01"),
+        end: Temporal.PlainDate.from("2027-07-01"),
+        dependsOn: 301,
+      },
+      {
+        id: 303,
+        name: "Conversione per ebook",
+        start: Temporal.PlainDate.from("2027-06-01"),
+        end: Temporal.PlainDate.from("2027-08-01"),
+        dependsOn: 301,
+      },
+      {
+        id: 304,
+        name: "Pubblicazione ebook",
+        start: Temporal.PlainDate.from("2027-08-01"),
+        end: Temporal.PlainDate.from("2027-09-01"),
+        dependsOn: 303,
+      },
+    ],
+  },
+];
